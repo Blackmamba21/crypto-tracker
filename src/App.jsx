d3359c2db20b1c90de2e4d57de7327188f74c817
Blackmamba21 <rathorehr021@gmail.com>
2024-06-05 11:31:48 +0530

    useDebounce custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
+import "./App.css";
+import useDebounce from "./hooks/useDebounce";
 function App() {
-  const [count, setCount] = useState(0)
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", debouncedHandleChange);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleChange);
+    };
+  }, []);
+
+  const debouncedHandleChange = useDebounce(handleResize, 2000);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Checkout</h1>
+      <p>Window Size: {`${windowSize.width}   X  ${windowSize.height}`}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
