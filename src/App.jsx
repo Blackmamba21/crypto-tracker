4c7b7519338468ebfccb09b228c159bb4b28bd44
Blackmamba21 <rathorehr021@gmail.com>
2024-06-06 17:01:57 +0530

    zustand with cart done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,70 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
-import useCounterStore from "./store";
+import { useCartStore } from "./store";
 function App() {
-  const { count, increment, decrement, incrementAsync } = useCounterStore();
+  const [inputs, setInputs] = useState({});
+
+  const { cart, loading, error, getProducts, deleteProduct, updateProduct } =
+    useCartStore();
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  const handleChange = (e, id) => {
+    setInputs({ ...inputs, [id]: e.target.value }); // Update the value for the specific item
+  };
+
+  const handleUpdate = (id) => {
+    const inputValue = inputs[id]; // Get the value for the specific item
+    let payload = cart.find((item) => item.id === id);
+    console.log("🚀 ~ handleUpdate ~ payload:", payload);
+    payload.title = inputValue;
+    updateProduct(id, payload); // Update the product
+    setInputs({ ...inputs, [id]: null });
+  };
+
   return (
     <>
       <h1>Zustand</h1>
-      <div style={{ display: "flex", justifyContent: "center", gap: "8px" }}>
-        <button onClick={increment}>Increment</button>
-        <p>{count}</p>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={incrementAsync}>Increment Async</button>
+      <div>
+        <ul
+          style={{
+            background: "pink",
+            display: "flex",
+            flexDirection: "column",
+            gap: "8px",
+          }}
+        >
+          {cart.map((item) => (
+            <div
+              style={{
+                background: "yellow",
+                display: "flex",
+                gap: "3px",
+              }}
+            >
+              <li key={item.id}>{item.title}</li>
+              <input
+                key={item.id}
+                type="text"
+                value={inputs[item.id] || ""}
+                onChange={(e) => handleChange(e, item.id)}
+              />
+
+              <button onClick={() => handleUpdate(item.id)}>Update</button>
+              <button onClick={() => deleteProduct(item.id)}>Delete</button>
+            </div>
+          ))}
+        </ul>
       </div>
     </>
   );
--- src/store.js
+++ src/store.js
@@ -1,17 +1,105 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+export const useCounterStore = create(
+  persist(
+    (set) => ({
+      count: 0,
+      increment: () => {
+        set((state) => ({ count: state.count + 1 }));
+      },
+      incrementAsync: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        set((state) => ({ count: state.count + 1 }));
+      },
+      decrement: () => {
+        set((state) => ({ count: state.count - 1 }));
+      },
+    }),
+    {
+      name: "counter-storage", // unique name for the storage item
+      getStorage: () => localStorage, // specify the storage to use
+    }
+  )
+);
 
-const useCounterStore = create((set) => ({
-  count: 0,
-  increment: () => {
-    set((state) => ({ count: state.count + 1 }));
+async function getProducts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching the todo:", error);
+  }
+}
+
+async function deleteProduct(id) {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/" + id,
+      {
+        method: "DELETE",
+      }
+    );
+    console.log("🚀 ~ deleteProduct ~ response:", response);
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching the todo:", error);
+  }
+}
+
+async function updateProduct(id, payload) {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/" + id,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
+    console.log("🚀 ~ updateProduct ~ response:", response);
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching the todo:", error);
+  }
+}
+
+export const useCartStore = create((set) => ({
+  cart: [],
+  loading: false,
+  error: null,
+  getProducts: async () => {
+    set({ loading: true, error: null });
+    try {
+      const products = await getProducts();
+      set((state) => ({ cart: [...state.cart, ...products], loading: false }));
+    } catch (error) {
+      set({ error: "Failed to fetch product", loading: false });
+    }
   },
-  incrementAsync: async () => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    set((state) => ({ count: state.count + 1 }));
+  deleteProduct: async (id) => {
+    set({ loading: true, error: null });
+    try {
+      const product = await deleteProduct(id);
+      console.log("🚀 ~ deleteProduct: ~ product:", product);
+      set((state) => ({
+        cart: state.cart.filter((item) => item.id !== id),
+        loading: false,
+      }));
+    } catch (error) {
+      set({ error: "Failed to delete product", loading: false });
+    }
   },
-  decrement: () => {
-    set((state) => ({ count: state.count - 1 }));
+  updateProduct: async (id, payload) => {
+    set({ loading: true, error: null });
+    try {
+      const product = await updateProduct(id, payload);
+      console.log("🚀 ~ updateProduct: ~ product:", product);
+
+      set((state) => ({ cart: [...state.cart, product], loading: false }));
+    } catch (error) {
+      set({ error: "Failed to update product", loading: false });
+    }
   },
 }));
-
-export default useCounterStore;
